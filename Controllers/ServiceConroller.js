489a13b5c2bee94382a88b0d6f589d6fbbaa0024
Nuh Genç <nuh16358@gmail.com>
2023-12-18 23:31:31 +0300

    servicesss

--- Controllers/ServiceConroller.js
+++ Controllers/ServiceConroller.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const { sidebar, kullanicilar } = require("./pageSettings");
-const {service_status,Service} = require('../Process/Services');
-
-
-
+const { service_status, Service } = require("../Process/Services");
 
 exports.ServiceController = async (req, res) => {
-
-res.render("Pages/Services", {
-    services : await service_status(),
+  res.render("Pages/Services", {
+    services: await service_status(),
     pages: sidebar,
     kullanicilar,
-  }); 
-
-
-
- 
+  });
 };
- 
 
+exports.ServiceRestart = async (req, res) => {
+  res.json(await Service.Restart(req.params.service_name));
+};
+
+exports.ServiceStart = async (req, res) => {
+  res.json(await Service.Start(req.params.service_name));
+};
+
+exports.ServiceStop = async (req, res) => {
+  res.json(await Service.Stop(req.params.service_name));
+};
--- Routers/ServiceRoute.js
+++ Routers/ServiceRoute.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const {ServiceController} = require('../Controllers/ServiceConroller')
+const {ServiceController,ServiceRestart,ServiceStart,ServiceStop} = require('../Controllers/ServiceConroller')
 
 
 
 router.get('/services', ServiceController);
+router.get('/service/restart/:service_name', ServiceRestart);
+router.get('/service/start/:service_name',ServiceStart );
+router.get('/service/stop/:service_name', ServiceStop);
 
 
 module.exports = router
